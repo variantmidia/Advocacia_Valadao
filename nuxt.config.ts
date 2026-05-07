bca37af826e91d289efbe9c55d48a4d8af457cdf
variantmidia <mitobrmatanob@gmail.com>
2026-05-07 17:37:20 -0300

    Add Google Tag Manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,17 @@ export default defineNuxtConfig({
         { name: 'robots', content: 'index, follow' },
         { name: 'theme-color', content: '#080808' },
       ],
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KDQ72J8N');`,
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDQ72J8N" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          tagPosition: 'bodyOpen',
+        },
+      ],
       link: [
         { rel: 'canonical', href: 'https://advogadowagnervaladao.com.br' },
         { rel: 'icon', type: 'image/png', href: '/images/logo.png' },
